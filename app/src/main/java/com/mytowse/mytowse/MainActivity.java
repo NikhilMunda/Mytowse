aa69cf38618da615571df50d435732125ae721ee
NIKHIL <nikhilmunda@gmail.com>
2022-05-13 12:05:25 +0530

    Changes Added

---
+++ app/src/main/java/com/mytowse/mytowse/MainActivity.java
@@ -0,0 +1,37 @@
+package com.mytowse.mytowse;
+
+import static com.mytowse.mytowse.R.color.nikhil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(nikhil));
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i=new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
